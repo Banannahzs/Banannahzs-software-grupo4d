c8012f1bef31f6b330a9fc99e0de13e97fb87889
Hannah <hannah.martins.vianna@gmail.com>
2023-10-18 10:30:30 -0300

    initial commit

---
+++ src/pages/Primeiro.jsx
@@ -0,0 +1,16 @@
+import LinkSegundo from "../components/LinkSegundo";
+import LinkGenerico from "../components/LinkGenerico";
+
+const Primeiro = () => {
+    return (
+        <div>
+            <h5>Meu titulo</h5>
+             <p>Meu texto</p>
+             <LinkSegundo /> <br/>
+             <LinkGenerico />
+         </div>
+        );
+
+};
+
+export default Primeiro;
